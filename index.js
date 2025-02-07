ae93b467fb8c2a950614fcdcec01e319f841925d
rafaabc <faelsabc21@gmail.com>
2025-02-07 12:36:28 +0100

    Minor refactor to avoid duplicated code

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ module.exports = function calculateDiscount(products) {
     .map((product) => {
       const { uniqueId, unitPrice, quantity } = product;
       if (uniqueId === "PROMO-LOJINHA") foundSpecialProduct = true;
-      return unitPrice * quantity;
+      return multiply(unitPrice, quantity);
     })
     .reduce((acc, value) => acc + value, 0);
-  if (foundSpecialProduct) return totalValue * 0.2;
-  if (totalValue > 0 && totalValue <= 499.99) return totalValue * 0.05;
-  if (totalValue >= 500 && totalValue <= 999.99) return totalValue * 0.1;
-  if (totalValue >= 1000) return totalValue * 0.15;
+  if (foundSpecialProduct) return multiply(totalValue, 0.2);
+  if (totalValue > 0 && totalValue <= 499.99) return multiply(totalValue, 0.05);
+  if (totalValue >= 500 && totalValue <= 999.99) return multiply(totalValue, 0.1);
+  if (totalValue >= 1000) return multiply(totalValue, 0.15);
   return 0;
 };
+
+function multiply(val1, val2) {
+  return val1 * val2;
+}
