b0b1992afa6b7a044117da958915a1f16f81f735
rafaabc <faelsabc21@gmail.com>
2025-02-07 12:00:20 +0100

    Make the test pass and refactor

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 module.exports = function calculateDiscount(products) {
-  if (!products.length) return 0;
-  if (products.length === 1) {
-    const { unitPrice, quantity } = products[0];
-    const totalValue = unitPrice * quantity;
-    if (products[0].uniqueId === "PROMO-LOJINHA") return totalValue * 0.2;
-    if (totalValue > 0 && totalValue <= 499.99) return totalValue * 0.05;
-    if (totalValue >= 500 && totalValue <= 999.99) return totalValue * 0.1;
-    if (totalValue >= 1000) return totalValue * 0.15;
-  }
+  let foundSpecialProduct = false;
+  const totalValue = products
+    .map((product) => {
+      const { uniqueId, unitPrice, quantity } = product;
+      if (uniqueId === "PROMO-LOJINHA") foundSpecialProduct = true;
+      return unitPrice * quantity;
+    })
+    .reduce((acc, value) => acc + value, 0);
+  if (foundSpecialProduct) return totalValue * 0.2;
+  if (totalValue > 0 && totalValue <= 499.99) return totalValue * 0.05;
+  if (totalValue >= 500 && totalValue <= 999.99) return totalValue * 0.1;
+  if (totalValue >= 1000) return totalValue * 0.15;
   return 0;
 };
