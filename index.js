2cf8bd3c7da57810f819d9f5c98ca0c5ede9bd53
rafaabc <faelsabc21@gmail.com>
2025-02-07 11:34:42 +0100

    Make test pass

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = function calculateDiscount(products) {
   if (!products.length) return 0;
-  const { unitPrice, quantity } = products[0];
-  if (unitPrice > 0 && unitPrice <= 499.99) return unitPrice * quantity * 0.05;
-  if (unitPrice >= 500 && unitPrice <= 999.99)
-    return unitPrice * quantity * 0.1;
+  if (products.length === 1) {
+    const { unitPrice, quantity } = products[0];
+    const totalValue = unitPrice * quantity;
+    if (totalValue > 0 && totalValue <= 499.99) return totalValue * 0.05;
+    if (totalValue >= 500 && totalValue <= 999.99) return totalValue * 0.1;
+    if (totalValue >= 1000) return totalValue * 0.15;
+  }
+  return 0;
 };
